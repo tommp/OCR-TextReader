3d9b1543ad2a651883d6116c8e1e1f2346465579
tommp <tomsju@hotmail.com>
2015-02-27 02:36:03 +0100

    Finished greyscale, gaussian smoothing and gradient magnitude calculations

--- source/headers/main.hpp
+++ source/headers/main.hpp
@@ -22,13 +22,62 @@
 #include <unistd.h>
 /*---------------------------------------------*/
 
+using namespace cimg_library;
+
 namespace SDLconsts {
 	const unsigned int window_length = 500;
 	const unsigned int window_height = 500;
 }
 
+namespace CImgconsts {
+	const float RED_SCALE = 0.21;
+	const float GREEN_SCALE = 0.71;
+	const float BLUE_SCALE = 0.07;
+	const int GAUSSIAN_SIZE = 5;
+
+	/* Discrete approximation of a 2D gaussian function, stolen from wikipedia, standard deviation is  0.84089642 */
+	/* This is not neccessarily optimal, room for improvement here */
+	const float GAUSSIAN[5][5] = {
+		{0.00078634, 0.00655965, 0.01330373, 0.00655965, 0.00078633},
+		{0.00655965, 0.05472157, 0.11098164, 0.05472157, 0.00655965},
+		{0.01330373, 0.11098164, 0.22508352, 0.11098164, 0.01330373},
+		{0.00655965, 0.05472157, 0.11098164, 0.05472157, 0.00655965},
+		{0.00078633, 0.00655965, 0.01330373, 0.00655965, 0.00078633}
+	};
+
+	/* Convolution mask (x direction) for gradient calculation (sobel operator) */
+	const int GX[3][3] = {
+		{-1, 0, 1},
+		{-2, 0, 2},
+		{-1, 0, 1}
+	};
+
+	/* Convolution mask (y direction) for gradient calculation (sobel operator) */
+	const int GY[3][3] = {
+		{-1, -2, -1},
+		{0, 0, 0},
+		{1, 2, 1}
+	};
+	const int GAUSSIAN_OFFSET_FROM_CENTER = 2;
+}
+	
+
+
+
+
+
 /*Header content*/
 /*=============================================*/
+//LUMINOSITY METHOD: (0.21 R + 0.71 G + 0.07 B)
+void convert_to_greyscale(CImg<unsigned char>& image, CImg<unsigned char>& gray);
+void apply_gaussian_smoothing(CImg<unsigned char>& grayimage);
+void calculate_gradient_magnitude_and_direction(CImg<unsigned char>& grayimage, 
+												CImg<unsigned char>& direction, 
+												CImg<double>& magnitude);
+void apply_non_maximum_suppress(CImg<unsigned char>& grayimage, 
+								CImg<unsigned char>& direction, 
+								CImg<double>& magnitude);
+
 void pollevent(bool& var);
 
 void waitForEvent();
--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,9 @@
 #include "./headers/main.hpp"
-using namespace cimg_library;
+
+
 
 int main(int argc, char** argv){
+
 	/*Initializes SDL for graphical display*/
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
 		SDLerrorLogger("SDL initialization");
@@ -12,38 +14,213 @@ int main(int argc, char** argv){
 	/*Disables pesky screensavers while our wonderful graphics are beeing displayed*/
 	SDL_DisableScreenSaver();
 
-	/*Initializes a window to render graphics in*/
+	/*Initializes a window to render graphics in*//*
 	SDL_Window *win = SDL_CreateWindow("EM", 0, 0, SDLconsts::window_length, SDLconsts::window_height, 0);
 	if (win == nullptr){
 		SDLerrorLogger("SDL_CreateWindow");
 		std::cout<<"Failed to create SDL window, see errorlog for details."<<std::endl;
 		return 1;
-	}
+	}*/
 
-	/*Initializes the renderer to draw in*/
+	/*Initializes the renderer to draw in*//*
 	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (ren == nullptr){
 		SDLerrorLogger("SDL_CreateRenderer");
 		std::cout<<"Failed to create SDL renderer, see errorlog for details."<<std::endl;
 		return 1;
-	}
+	}*/
 
-	CImg<unsigned char> image("../data/normalShadow.JPG"), visu(500,400,1,3,0);
-	const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
-		image.blur(2.5);
-	CImgDisplay main_disp(image,"Click a point"), draw_disp(visu,"Intensity profile");
-	while (!main_disp.is_closed() && !draw_disp.is_closed()) {
-		main_disp.wait();
-		if (main_disp.button() && main_disp.mouse_y()>=0) {
-			const int y = main_disp.mouse_y();
-			visu.fill(0).draw_graph(image.get_crop(0,y,0,0,image.width()-1,y,0,0),red,1,1,0,255,0);
-			visu.draw_graph(image.get_crop(0,y,0,1,image.width()-1,y,0,1),green,1,1,0,255,0);
-			visu.draw_graph(image.get_crop(0,y,0,2,image.width()-1,y,0,2),blue,1,1,0,255,0).display(draw_disp);
-		}
-	}
+	CImg<unsigned char> image(argv[1]);
+	CImg<unsigned char> grayscale1(image.width(), image.height(), image.depth(), 1);
+	CImg<unsigned char> grayscale2(image.width(), image.height(), image.depth(), 1);
+	CImg<unsigned char> direction(image.width(), image.height(), image.depth(), 1);
+	CImg<double> magnitude(image.width(), image.height(), image.depth(), 1);
+	//convert_to_greyscale(image, grayscale1);
+	convert_to_greyscale(image, grayscale2);
+	apply_gaussian_smoothing(grayscale2);
+
+	calculate_gradient_magnitude_and_direction(grayscale2, 
+												direction, 
+												magnitude);
+
+	
+	grayscale1.save("grayscale1.bmp");
+	grayscale2.save("grayscale2.bmp");
+	magnitude.save("magnitude.bmp");
+	direction.save("direction.bmp");
+		
+	(image,grayscale2, direction, magnitude).display("RGB to Grayscale");
 
 	waitForEvent();
 	
+}
+
+void apply_non_maximum_suppress(CImg<unsigned char>& grayimage, 
+								CImg<unsigned char>& direction, 
+								CImg<double>& magnitude){
+
+		for (int x = 1; x < grayimage.width()-1; x++){
+			for (int y = 1; y < grayimage.height()-1; y++){
+				switch((int)direction(x,y)){
+					case 0:
+						if(magnitude(x+1,y) > magnitude(x,y)){
+							grayimage(x,y) = 0;
+						}
+						break;
+					case 1:
+						if(magnitude(x+1,y-1) > magnitude(x,y)){
+							grayimage(x,y) = 0;
+						}
+						break;
+					case 2:
+						if(magnitude(x,y-1) > magnitude(x,y)){
+							grayimage(x,y) = 0;
+						}
+						break;
+					case 3:
+						if(magnitude(x-1,y-1) > magnitude(x,y)){
+							grayimage(x,y) = 0;
+						}
+						break;
+					case 4:
+						if(magnitude(x-1,y) > magnitude(x,y)){
+							grayimage(x,y) = 0;
+						}
+						break;
+					case 5:
+						if(magnitude(x-1,y+1) > magnitude(x,y)){
+							grayimage(x,y) = 0;
+						}
+						break;
+					case 6:
+						if(magnitude(x,y+1) > magnitude(x,y)){
+							grayimage(x,y) = 0;
+						}
+						break;
+					case 7:
+						if(magnitude(x+1,y+1) > magnitude(x,y)){
+							grayimage(x,y) = 0;
+						}
+						break;
+				}
+			}
+		}
+}
+
+void calculate_gradient_magnitude_and_direction(CImg<unsigned char>& grayimage, 
+												CImg<unsigned char>& direction, 
+												CImg<double>& magnitude) {
+
+	CImg<unsigned char> dx(grayimage.width(), grayimage.height(), grayimage.depth(), 1);
+	CImg<unsigned char> dy(grayimage.width(), grayimage.height(), grayimage.depth(), 1);
+	double aproxximate_direction = 0.0;
+	for (int x = 1; x < grayimage.width()-1; x++){
+		for (int y = 1; y < grayimage.height()-1; y++){
+			dx(x,y) = grayimage(x+1, y-1 ) * CImgconsts::GX[2][0] - grayimage(x-1, y-1 ) * CImgconsts::GX[0][0] + 
+						grayimage(x+1, y ) * CImgconsts::GX[2][1] - grayimage(x-1, y ) * CImgconsts::GX[0][1] + 
+						grayimage(x+1, y+1 ) * CImgconsts::GX[2][2] - grayimage(x-1, y+1 ) * CImgconsts::GX[0][2];
+
+			dy(x,y) = grayimage(x+1, y-1 ) * CImgconsts::GX[0][2] - grayimage(x-1, y-1 ) * CImgconsts::GX[0][0] + 
+						grayimage(x+1, y ) * CImgconsts::GX[1][2] - grayimage(x-1, y ) * CImgconsts::GX[1][0] + 
+						grayimage(x+1, y+1 ) * CImgconsts::GX[2][2] - grayimage(x-1, y+1 ) * CImgconsts::GX[2][0];
+
+			magnitude(x,y) = sqrt((double)dx(x,y)*(double)dx(x,y) + (double)dy(x,y)*(double)dy(x,y));
+
+
+			// [0, 45), [45, 90), [90, 135), [135,180), [180, 225), [225, 270), [270, 315), [315, 360)
+			// [337, 22), [22, 67), [67, 112), [12,157), [157, 202), [202, 247), 
+			//[247, 292), [292, 337)
+			// [89.82, 87.39), [--, 89.14), [--, 89.48), [--, 89.63), [--, 89.71), [--, 89.76), 
+			//[--, 89.80), [--, 89.82)
+
+			//in radians: [1.567654, 1.525243), [--, 1.555786), [--, 1.561720), [--, 1.564338), [--, 1.565734), [--, 1.566607), 
+			//[--, 1.567305), [--, 1.567654)
+			if (dy(x,y) == 0) {
+				aproxximate_direction = 0.0;
+			}
+			else if (dx(x,y) == 0) {
+				aproxximate_direction = atan((double)dy(x,y)/0.1);
+			}
+			else{
+				aproxximate_direction = atan((double)dy(x,y)/(double)dx(x,y));
+			}
+
+			if(aproxximate_direction < 1.525243) {
+				direction(x,y) = 0;
+			}
+			else if(aproxximate_direction >= 1.525243 && aproxximate_direction < 1.555786) {
+				direction(x,y) = 10;//45
+			}
+			else if(aproxximate_direction >= 1.555786 && aproxximate_direction < 1.561720) {
+				direction(x,y) = 20;//90
+			}
+			else if(aproxximate_direction >= 1.561720 && aproxximate_direction < 1.564338) {
+				direction(x,y) = 30;//135
+			}
+			else if(aproxximate_direction >= 1.564338 && aproxximate_direction < 1.565734) {
+				direction(x,y) = 40;//180
+			}
+			else if(aproxximate_direction >= 1.565734 && aproxximate_direction < 1.566607) {
+				direction(x,y) = 50;//225
+			}
+			else if(aproxximate_direction >= 1.566607 && aproxximate_direction < 1.567305) {
+				direction(x,y) = 60;//270
+			}
+			else if(aproxximate_direction >= 1.567305 && aproxximate_direction < 1.567654) {
+				direction(x,y) = 70;//315
+			}
+			else if(aproxximate_direction >= 1.567654) {
+				direction(x,y) = 80;//360
+			}
+		}
+	}
+}
+
+void apply_gaussian_smoothing(CImg<unsigned char>& grayimage) {
+
+	float gaussian_sum = 0.0;
+
+	for (int x = 0; x < CImgconsts::GAUSSIAN_SIZE; x++) {
+		for (int y = 0; y < CImgconsts::GAUSSIAN_SIZE; y++) {
+			gaussian_sum += CImgconsts::GAUSSIAN[x][y];
+		} 
+	}
+
+	float pixel_sum = 0.0;
+
+	for (int i = 0; i < grayimage.width(); i++){
+		for (int j = 0; j < grayimage.height(); j++){
+			pixel_sum = 0.0;
+			for (int k = -CImgconsts::GAUSSIAN_OFFSET_FROM_CENTER; k < CImgconsts::GAUSSIAN_OFFSET_FROM_CENTER; k++){
+				for (int l = -CImgconsts::GAUSSIAN_OFFSET_FROM_CENTER; l < CImgconsts::GAUSSIAN_OFFSET_FROM_CENTER; l++) {
+					if (i+k > grayimage.width() || i+k < 0 || j+l > grayimage.height() || j+l < 0) {
+						continue;
+					}
+					else{
+						pixel_sum += (grayimage(i + k, j + l) * 
+							CImgconsts::GAUSSIAN[k + CImgconsts::GAUSSIAN_OFFSET_FROM_CENTER]
+												[l + CImgconsts::GAUSSIAN_OFFSET_FROM_CENTER]);
+					}
+				}
+			}
+			grayimage(i, j) = round(pixel_sum/gaussian_sum);
+		}
+	}
+}
+
+/* Converts an image to grayscale using the luminocity method, it puts extra weight on green */
+void convert_to_greyscale(CImg<unsigned char>& image, CImg<unsigned char>& gray){
+	unsigned char red, green, blue;
+	for (int x = 0; x < image.width(); x++){
+		for (int y = 0; y < image.height(); y++){
+			red = image(x,y,0, 0);
+			green = image(x,y,0, 1);
+			blue = image(x,y,0, 2);
+			gray(x,y) = round(((float)red * CImgconsts::RED_SCALE + 
+				(float)green * CImgconsts::GREEN_SCALE + 
+				(float)blue * CImgconsts::BLUE_SCALE)/3);
+		}
+	}
 }
 
 void pollevent(bool& var){
